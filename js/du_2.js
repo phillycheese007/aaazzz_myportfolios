b3d7041c477cf1b3df1be67e332a60ce16fd845e
phillycheese007 <philipdusel@gmail.com>
2017-03-11 11:40:19 -0500

    Update du_2.js

--- js/du_2.js
+++ js/du_2.js
@@ -52,9 +52,27 @@ loader.load('images/deer/pot.js', function (geometry) {
     subj.scale.set(20,20,20);
     subj.rotation.y = convertToRad(90);
     scene.add(subj);
-    
-  
 });
+
+
+	// Create an array of materials to be used in a cube, one for each side
+	var cubeMaterialArray = [];
+	// order to add materials: x+,x-,y+,y-,z+,z-
+	cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0xff3333 } ) );
+	cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0xff8800 } ) );
+	cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0xffff33 } ) );
+	cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0x33ff33 } ) );
+	cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0x3333ff } ) );
+	cubeMaterialArray.push( new THREE.MeshBasicMaterial( { color: 0x8833ff } ) );
+	var cubeMaterials = new THREE.MeshFaceMaterial( cubeMaterialArray );
+	// Cube parameters: width (x), height (y), depth (z), 
+	//        (optional) segments along x, segments along y, segments along z
+	var cubeGeometry = new THREE.CubeGeometry( 100, 100, 100, 1, 1, 1 );
+	// using THREE.MeshFaceMaterial() in the constructor below
+	//   causes the mesh to use the materials stored in the geometry
+	cube = new THREE.Mesh( cubeGeometry, cubeMaterials );
+	cube.position.set(-100, 50, -50);
+	scene.add( cube );
 
 
 var spotlight = new THREE.PointLight(0xffffff);
