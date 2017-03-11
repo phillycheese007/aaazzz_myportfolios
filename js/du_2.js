27943647097184f54f533100f95842e27ba0b837
phillycheese007 <philipdusel@gmail.com>
2017-03-11 09:27:51 -0500

    Update du_2.js

--- js/du_2.js
+++ js/du_2.js
@@ -75,7 +75,8 @@ scene.add(light2);
 //
 var render = function () {
   requestAnimationFrame( render );
-
+				camera.position.x += ( mouseX - camera.position.x ) * .05;
+				camera.position.y += ( - mouseY - camera.position.y ) * .05;
   update();
 
   renderer.render(scene, camera);
