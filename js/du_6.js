5482f571c7b5a8d5f9c974c5fa47aec0ea2cdaee
phillycheese007 <philipdusel@gmail.com>
2017-04-09 15:20:32 -0400

    Update du_6.js

--- js/du_6.js
+++ js/du_6.js
@@ -121,7 +121,7 @@ loader.load('images/deer/pot.js', function (geometry) {
     subj.castShadow = true;
     geometry.computeVertexNormals();
     subj.scale.set(5,5,5);
-	subj.position.set(10, 0, 50);
+	subj.position.set(300, 0, 50);
 subj.castShadow = true;
     subj.rotation.y = convertToRad(90);
     scene.add(subj);
@@ -137,7 +137,7 @@ loader.load('images/deer/bean2.js', function (geometry) {
     subj.castShadow = true;
     geometry.computeVertexNormals();
     subj.scale.set(20,20,20);
-	subj.position.set(-70, 0, 50);
+	subj.position.set(0, 0, 50);
 subj.castShadow = true;
     subj.rotation.y = convertToRad(90);
     scene.add(subj);
