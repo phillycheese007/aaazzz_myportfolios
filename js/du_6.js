829ff9314a1cb466564fdb3f504076f1a2d67d4a
phillycheese007 <philipdusel@gmail.com>
2017-04-10 07:54:34 -0400

    Update du_6.js

--- js/du_6.js
+++ js/du_6.js
@@ -46,8 +46,8 @@ function addLights() {
   // var dirLight = new THREE.DirectionalLight(0xffffff, 1);
   //  dirLight.position.set(-500, 100, 50);
  //  scene.add(dirLight);
-//var ambLight = new THREE.AmbientLight(0x404040);
-//scene.add(ambLight);
+var ambLight = new THREE.AmbientLight(0x404040);
+scene.add(ambLight);
   
 var bluePoint = new THREE.PointLight(0x0033ff, 3, 150);
 bluePoint.position.set( 70, 5, 270 );
@@ -73,9 +73,9 @@ spotLight.penumbra = 0.8;
 scene.add(spotLight);
 scene.add(new THREE.PointLightHelper(spotLight, 1));	
 	
-//var hemLight = new THREE.HemisphereLight(0xffe5bb, 0xFFBF00, .1);
-//scene.add(hemLight);	
-//}
+var hemLight = new THREE.HemisphereLight(0xffe5bb, 0xFFBF00, .1);
+scene.add(hemLight);	
+}
 
 
 function addSceneElements() {
