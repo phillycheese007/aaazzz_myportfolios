f4a2e71bd4b16477654155e38f50df5f79341db6
phillycheese007 <philipdusel@gmail.com>
2017-04-16 08:35:40 -0400

    Update cloth.js

--- please_work/js/cloth.js
+++ please_work/js/cloth.js
@@ -20,7 +20,7 @@ var restDistance = 25;
 var xSegs = 10;
 var ySegs = 10;
 
-var clothFunction = plane( restDistance / xSegs, restDistance * ySegs );
+var clothFunction = plane( restDistance * xSegs, restDistance / ySegs );
 
 var cloth = new Cloth( xSegs, ySegs );
 
